e8788b0d528bde893da60adab9cfa0ac5be06848
â1154978352 <1154978352@qq.com>
2018-02-21 20:46:58 +0800

    新增redis

---
+++ spring-boot-redis/src/main/java/com/gwd/controller/TestController.java
@@ -0,0 +1,29 @@
+package com.gwd.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.gwd.dao.RedisDao;
+
+/** 
+* @ProjectName spring-boot-redis
+* @author JackHisen(gu.weidong) 
+* @Date 2018年2月21日 下午8:00:27    
+* @Version 1.0 
+* @Description:
+*/
+@RestController
+public class TestController {
+	@Autowired
+    RedisDao redisDao;
+    @RequestMapping("/testRedis")
+    public void testRedis(){
+    	System.out.println("------------------------");
+        redisDao.setKey("name","forezp");
+        redisDao.setKey("age","11");
+       System.out.println(redisDao.getValue("name"));
+       System.out.println(redisDao.getValue("age"));
+    }
+}
+
---
+++ spring-boot-redis/src/main/java/com/gwd/dao/RedisDao.java
@@ -0,0 +1,32 @@
+package com.gwd.dao;
+
+import java.util.concurrent.TimeUnit;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
+import org.springframework.stereotype.Repository;
+
+/** 
+* @ProjectName spring-boot-redis
+* @author JackHisen(gu.weidong) 
+* @Date 2018年2月21日 下午7:59:17    
+* @Version 1.0 
+* @Description:
+*/
+@Repository
+public class RedisDao {
+	@Autowired
+    private StringRedisTemplate template;
+
+    public  void setKey(String key,String value){
+        ValueOperations<String, String> ops = template.opsForValue();
+        ops.set(key,value,1, TimeUnit.MINUTES);//1分钟过期
+    }
+
+    public String getValue(String key){
+        ValueOperations<String, String> ops = this.template.opsForValue();
+        return ops.get(key);
+    }
+}
+
