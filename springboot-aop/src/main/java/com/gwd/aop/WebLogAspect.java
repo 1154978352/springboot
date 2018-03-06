6f6f86a3c264a996deff6260acf5a098e424ec55
guweidong <1154978352@qq.com>
2018-03-06 14:33:18 +0800

    新增aop

---
+++ springboot-aop/src/main/java/com/gwd/aop/WebLogAspect.java
@@ -0,0 +1,49 @@
+package com.gwd.aop;
+
+import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.stereotype.Component;
+
+/** 
+* @FileName WebLogAspect.java
+* @Description:TODO
+* @author JackHisen(gu.weidong)
+* @version V1.0
+* @createtime 2018年2月27日 上午9:32:17 
+* 修改历史：
+* 时间           作者          版本        描述
+*====================================================  
+*
+*/
+@Component
+@Aspect
+public class WebLogAspect {
+	@Pointcut("execution(public * com.gwd.web..*.*(..))")
+	public void weblog() {}
+	
+	@Before("weblog()")
+	public void webBefore() {
+		System.out.println("在更新之前");
+		System.out.println("stop");
+		return;
+	}
+	
+	@After("weblog()")
+	public void webAfter() {
+		System.out.println("在更新之后");
+	}
+	
+	@Around("weblog()")
+	public void webAround() {
+		System.out.println("环绕通知……");
+	}
+	
+	@AfterReturning("weblog()")
+	public void webAfterReturn() {
+		System.out.println("afterReturning ……");
+	}
+}
---
+++ springboot-aop/src/main/java/com/gwd/web/TestController.java
@@ -0,0 +1,24 @@
+package com.gwd.web;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/** 
+* @FileName TestController.java
+* @Description:TODO
+* @author JackHisen(gu.weidong)
+* @version V1.0
+* @createtime 2018年2月27日 上午10:03:26 
+* 修改历史：
+* 时间           作者          版本        描述
+*====================================================  
+*
+*/
+@RestController
+public class TestController {
+	@RequestMapping("/testAop")
+	public void testAop() {
+		System.out.println("test进行中");
+		return ;
+	}
+}
